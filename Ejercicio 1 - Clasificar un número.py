4ec196ed754f83319cfeaa02e6893eaf4838ca17
Manuel Vargas <manuelevc94@hotmail.com>
2026-06-10 22:54:25 -0500

    Ejercicio 1
    Clasificar un numero - el código muestra si un numero es 0 , positivo o negativo.

---
+++ Ejercicio 1 - Clasificar un número.py
@@ -0,0 +1,11 @@
+#Ingreso de datos
+num = int(input("Ingrese un numero entero : "))
+
+#Evaluación y muestra de resultado
+if num == 0:
+    print("El numero ingresado es 0")
+elif num>0:
+    print("El numero ingresado es positivo")
+else:
+    print("El numero ingresado es negativo")
+    
